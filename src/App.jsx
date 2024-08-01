7712f551f5b35ed704aec18751716c9f554b52cc
kodegv <celsocharlez1@gmail.com>
2024-08-01 16:46:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+import Meme from "../components/Meme"
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+      <Header/>
+      <Meme/>
+    </div>
   )
 }
 
